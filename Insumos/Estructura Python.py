44df4317564ac993d7a2af0cda134f8a802fb403
Josmar Gustavo Palomino Castelan <jpalomino@valperia.mx>
2026-02-18 18:02:13 -0600

    Insumos
    Insumos ejercicios

---
+++ Insumos/Estructura Python.py
@@ -0,0 +1,16 @@
+# 1. Definición de la función
+def ejemplo_estructura_python():
+
+    # 2. Importación de librerías
+    import os
+
+    # 3. Variable recibida desde Rocketbot
+    archivo_entrada = r"{str_PathArchivo}"
+
+    # 4. Cuerpo / lógica del proceso (ejemplo)
+    print("Iniciando proceso...")
+    print(f"Archivo recibido: {archivo_entrada}")
+    print("Ejecutando lógica del bot...")
+
+# 5. Llamado de la función
+ejemplo_estructura_python()
